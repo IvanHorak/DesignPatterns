2440522dc592c0744a86cb6c1d72272531fba0a8
eivahor <ivan.horak@ericsson.com>
2019-09-26 16:42:45 +0200

    Add Composite pattern example

---
+++ src/main/java/structural/composite/RunCompositePattern.java
@@ -0,0 +1,18 @@
+package structural.composite;
+
+public class RunCompositePattern {
+    public static void main(String[] args) {
+        Menu mainMenu = new Menu("Main", "/main");
+        MenuItem safetyMenuItem = new MenuItem("Safety", "/safety");
+        mainMenu.add(safetyMenuItem);
+
+        Menu claimsSubMenu = new Menu("Claims", "/claims");
+        mainMenu.add(claimsSubMenu);
+
+        MenuItem personalClaimsMenu = new MenuItem("Personal Claim", "/personalClaims");
+        claimsSubMenu.add(personalClaimsMenu);
+
+        System.out.println(mainMenu.toString());
+
+    }
+}
